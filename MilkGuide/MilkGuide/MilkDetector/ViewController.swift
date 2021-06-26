93cace550f2e6114b7b5bb881aed964b310fd56c
heegeepark <phg980513@naver.com>
2021-06-26 10:52:10 +0900

    Feat: Initial Commit - font, img, icon 등 asset 추가 - tab bar controller scene 추가 - detector view storyboard 구현

---
+++ MilkGuide/MilkGuide/MilkDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilkGuide
+//
+//  Created by 박희지 on 2021/06/26.
+//
+
+import UIKit
+
+class MilkDetectorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

a2fe39b5bd6ae61521115eb6ddc46a9ca16d555a
heegeepark <phg980513@naver.com>
2021-08-01 16:44:40 +0900

    Feat: Add NetworkAPI 서버에 이미지 파일 전송 post메서드 추가

--- MilkGuide/MilkGuide/MilkDetector/NetworkAPI.swift
+++ MilkGuide/MilkGuide/MilkDetector/NetworkAPI.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 import UIKit
+import Alamofire
 
-class N
+// 바디 파라미터: {"image": filename}
+// response: {"class_name": string}
+class networkAPI {
+    static func requestClassification(_ image: UIImage) {
+        let url = "http://067aae530435.ngrok.io"
+        let imageData = image.jpegData(compressionQuality: 0.5)!
+        
+        AF.upload(multipartFormData: { multipartFormData in
+//            multipartFormData.append(Data("value".utf8), withName: "Key")
+            multipartFormData.append(imageData,withName: "image",fileName: "milk.jpg", mimeType: "image/jpg")
+        }, to: url).responseJSON { response in
+            print(response)
+        }
+        
+    }
+}

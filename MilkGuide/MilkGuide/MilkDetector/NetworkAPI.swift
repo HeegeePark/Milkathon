858bd82faa8b590f6005b2748bc1729ea8be193c
heegeepark <phg980513@naver.com>
2021-08-04 13:24:30 +0900

    Feat: Add Decoding JSON 이미지 업로드 부분 실패하지만, 일단 디코딩 코드 추가함

--- MilkGuide/MilkGuide/MilkDetector/NetworkAPI.swift
+++ MilkGuide/MilkGuide/MilkDetector/NetworkAPI.swift
@@ -9,19 +9,44 @@ import Foundation
 import UIKit
 import Alamofire
 
-// 바디 파라미터: {"image": filename}
+// 바디 파라미터: {"file": filename}
 // response: {"class_name": string}
 class networkAPI {
-    static func requestClassification(_ image: UIImage) {
-        let url = "http://067aae530435.ngrok.io"
+
+    static func requestClassification(_ image: UIImage) -> String {
+        let url = "http://57e8bb56e407.ngrok.io"
         let imageData = image.jpegData(compressionQuality: 0.5)!
+        var prediction = ""
         
         AF.upload(multipartFormData: { multipartFormData in
-//            multipartFormData.append(Data("value".utf8), withName: "Key")
-            multipartFormData.append(imageData,withName: "image",fileName: "milk.jpg", mimeType: "image/jpg")
+            multipartFormData.append(imageData, withName: "file", fileName: "milk.jpg", mimeType: "image/jpg")
         }, to: url).responseJSON { response in
-            print(response)
+            print(" 리스폰스: \(response)")
+            switch response.result {
+            case .success(let value):
+                print("성공 시 밸류: \(value)")
+                do {
+                    let data = try JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+                    prediction = try JSONDecoder().decode(Result.self, from: data).prediction
+                } catch let error {
+                    print("----------파싱에러-------------")
+                    print(error)
+                }
+                
+            case .failure(let error):
+                print("----------failure------------> \(error)")
+                prediction = "네트워크 실패"
+            }
         }
+        return prediction
+    }
+    
+}
+
+struct Result: Codable {
+    let prediction: String
         
+    enum CodeK: String, CodingKey {
+        case prediction = "class_name"
     }
 }
